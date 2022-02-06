99d5283070730d9ab84f787941b22f35bf13b6b5
Abhijeet B Kurade <64726465+abhikurade@users.noreply.github.com>
2022-02-06 12:15:55 +0530

    Checking commit from my MAC.

--- src/main/java/DisjointSet/DisjointSetClass.java
+++ src/main/java/DisjointSet/DisjointSetClass.java
@@ -2,6 +2,8 @@ package DisjointSet;
 
 public class DisjointSetClass {
     public static void main(String[] args) {
+
+        System.out.println("Checking commit from another computer.");
         
     }
 }
