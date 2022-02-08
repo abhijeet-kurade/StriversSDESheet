f8de5a74c6bc138ae5f331dc7bfee6eeba00d028
Abhijeet B Kurade <64726465+abhikurade@users.noreply.github.com>
2022-02-08 18:00:26 +0530

    subset sum new class created.

--- src/main/java/Recursion/Recursion.java
+++ src/main/java/Recursion/Recursion.java
@@ -11,46 +11,7 @@ public class Recursion {
         System.out.println(subsetIISort(arr));
     }
 
-    public static ArrayList<Integer> subsetSum(int[] arr){
-        ArrayList<Integer> sums = new ArrayList<>();
-        subSetSum(0, 0, arr, sums);
-        return sums;
-    }
-    public static void subSetSum(int idx, int currSum, int[] arr, ArrayList<Integer> sums){
-        if(idx == arr.length) {
-            sums.add(currSum);
-            return;
-        };
 
-        /* adding current number in the sum
-         * */
-        currSum += arr[idx];
-        sums.add(currSum);
-        subSetSum(idx+1, currSum, arr, sums);
-
-        /*removing current number from the sum
-         * */
-        currSum -= arr[idx];
-        subSetSum(idx+1, currSum, arr, sums);
-
-    }
-    public static void subSetSum1(int idx, int currSum, int[] arr, ArrayList<Integer> sums){
-
-        if(idx == arr.length) {
-            sums.add(currSum);
-            return;
-        };
-
-        /*removing current number from the sum
-         * */
-        subSetSum1(idx+1, currSum, arr, sums);
-
-        /* adding current number in the sum
-         * */
-        currSum += arr[idx];
-        subSetSum1(idx+1, currSum, arr, sums);
-
-    }
 
     public static ArrayList<ArrayList<Integer>> subsetsIIHashSet(int[] arr){
         HashSet<ArrayList<Integer>> sums = new HashSet<>();
